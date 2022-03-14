1a61c6200da37ca9266c98c4a065c78d12c01f3e
eswaribala <parameswaribala@gmail.com>
2022-03-14 13:39:43 +0530

    updated

---
+++ inventoryservice/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using inventoryservice.Models;
+
+namespace inventoryservice.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<Product> AddProduct(Product Product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(long ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(long ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProduct(Product Product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ inventoryservice/Repositories/SupplierRepository.cs
@@ -0,0 +1,32 @@
+﻿using inventoryservice.Models;
+
+namespace inventoryservice.Repositories
+{
+    public class SupplierRepository : ISupplierRepository
+    {
+        public Task<Supplier> AddSupplier(Supplier Supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSupplier(long SupplierId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Supplier> GetSupplier(long SupplierId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Supplier>> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Supplier> UpdateSupplier(Supplier Supplier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
