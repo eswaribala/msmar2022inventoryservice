704d089a23680db1855abfe92e77f60c38e1732f
eswaribala <parameswaribala@gmail.com>
2022-03-14 12:04:00 +0530

    updated

---
+++ inventoryservice/Contexts/InventoryContext.cs
@@ -0,0 +1,22 @@
+﻿using inventoryservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace inventoryservice.Contexts
+{
+    public class InventoryContext : DbContext
+    {
+
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasOne(p => p.Supplier);
+        }
+    }
+}
