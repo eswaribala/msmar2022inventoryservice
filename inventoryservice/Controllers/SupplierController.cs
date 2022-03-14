c67ba16c78e74d9dab83e2aff0e7041907dd043d
eswaribala <parameswaribala@gmail.com>
2022-03-14 14:38:16 +0530

    updated

--- inventoryservice/Controllers/SupplierController.cs
+++ inventoryservice/Controllers/SupplierController.cs
@@ -7,7 +7,8 @@ using System.Transactions;
 
 namespace inventoryservice.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class SupplierController : ControllerBase
     {
