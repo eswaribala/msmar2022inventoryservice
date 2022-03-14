ac5bdfeb1cdf5a807c2f29bd5e592b4ab4c74412
eswaribala <parameswaribala@gmail.com>
2022-03-14 13:06:19 +0530

    updated

--- inventoryservice/Models/Supplier.cs
+++ inventoryservice/Models/Supplier.cs
@@ -1,12 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace inventoryservice.Models
 {
     [Table("Supplier")]
     public class Supplier
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        [Column("Supplier_Id")]
         public long SupplierId { get; set; }
-        public string SupplierName { get; set; }
+        [Required]
+        [Column("Supplier_Name")]
+        [StringLength(100)]
+        public string? SupplierName { get; set; }
 
      
 
--- inventoryservice/Repositories/SupplierRepository.cs
+++ inventoryservice/Repositories/SupplierRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace inventoryservice.Repositories
+﻿using inventoryservice.Models;
+
+namespace inventoryservice.Repositories
 {
     public interface SupplierRepository
     {
+        Task<IEnumerable<Supplier>> GetSuppliers();
+        Task<Supplier> GetSupplier(long SupplierId);
+        Task<Supplier> AddSupplier(Supplier Supplier);
+        Task<Supplier> UpdateSupplier(Supplier Supplier);
+        void DeleteSupplier(long SupplierId);
     }
 }
