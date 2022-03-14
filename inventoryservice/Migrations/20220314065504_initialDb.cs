29e70abfbe15c6624293edf14ab98122e29eeaa0
eswaribala <parameswaribala@gmail.com>
2022-03-14 12:25:47 +0530

    updated

---
+++ inventoryservice/Migrations/20220314065504_initialDb.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inventoryservice.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                columns: table => new
+                {
+                    SupplierId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SupplierName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.SupplierId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Product_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Product_Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Product_Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOP = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Buffer_Level = table.Column<long>(type: "bigint", nullable: false),
+                    Unit_Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Supplier_Id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Product_Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Supplier_Supplier_Id",
+                        column: x => x.Supplier_Id,
+                        principalTable: "Supplier",
+                        principalColumn: "SupplierId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Supplier_Id",
+                table: "Product",
+                column: "Supplier_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Supplier");
+        }
+    }
+}
